0af0d532ffbea930f9938775f56d9227340e67cf
ian6495 <ian6495@gmail.com>
2016-09-12 20:26:19 -0500

    Update Onboarding.ps1

--- Onboarding.ps1
+++ Onboarding.ps1
@@ -6,6 +6,7 @@ $Firstname = Read-Host -Prompt "Input the user's First Name here"
 $LastName = Read-Host -Prompt "Input the user's Last Name here"
 $title = Read-Host -Prompt "Input the user's Title here"
 $ipExtension = Read-Host -Prompt "Input the user's phone extension here"
+$OU = OU=Users,DC=na,DC=domain,DC=com
 $randomgen = -join ((65..90) + (97..122) | Get-Random -Count 12 | % {[char]$_})
 $clonedusername = Read-Host -Prompt "Input the username to close group memberships from"
 $username= $FirstName + "." + $LastName
@@ -16,7 +17,7 @@ $password = $randomgen + "1!"
 $upn = "$samaccountname@domain.local"  
 
 #Create New Users' Account
-New-ADUser -Name $Fullname -SamAccountName "$samaccountname" -GivenName "$FirstName" -Surname "$LastName" -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -DisplayName "$Fullname" -Path "OU=Users,OU=AUTOMATION,OU=-Austin,DC=na,DC=drillinginfo,DC=com" -UserPrincipalName "$upn" -Title "$title" -EmailAddress "$email" -Enabled $true -OtherAttributes @{ipPhone="$ipExtension"}
+New-ADUser -Name $Fullname -SamAccountName "$samaccountname" -GivenName "$FirstName" -Surname "$LastName" -AccountPassword (ConvertTo-SecureString -AsPlainText $password -Force) -DisplayName "$Fullname" -Path "$OU" -UserPrincipalName "$upn" -Title "$title" -EmailAddress "$email" -Enabled $true -OtherAttributes @{ipPhone="$ipExtension"}
 
 #Prints the password onscreen for immediate use if necessary to access the account
 Write-Host $password
