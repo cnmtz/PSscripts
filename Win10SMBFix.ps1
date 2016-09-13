dafeb088420a6db045e1cd3b013a9c486426fbd2
Christian Martinez <ian6495@gmail.com>
2016-09-13 10:20:52 -0500

    Added documentation for variable usage

--- Win10SMBFix.ps1
+++ Win10SMBFix.ps1
@@ -3,9 +3,10 @@ $regpath = "HKLM:\SYSTEM\CurrentControlSet\Services\LanmanWorkstation\Parameters
 $regname = "AllowInsecureGuestAuth"
 $regvalue = "1"
 
+#Replace DOMAIN.LOCAL with your domain to ensure that the FQDN is queried when run
 
 Invoke-Command -ComputerName $Machine -ScriptBlock { 
-sc.exe config lanmanworkstation depend= bowser/mrxsmb10/nsi ; sc.exe config mrxsmb20 start= disabled } | ForEach-Object{ "$_.na.drillinginfo.com"
+sc.exe config lanmanworkstation depend= bowser/mrxsmb10/nsi ; sc.exe config mrxsmb20 start= disabled } | ForEach-Object{ "$_.DOMAIN.LOCAL"
 New-ItemProperty -Path $regpath -Name $regname -Value $regvalue -PropertyType DWORD -Force | Out-Null
 Set-ItemProperty -Path $regpath -Name "EnablePlainTextPassword" -Value "0"
 Set-ItemProperty -Path $regpath -Name "EnableSecuritySignature" -Value "1"
