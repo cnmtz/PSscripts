2351255b868a807870a9d287c873267a7a8c261c
Christian Martinez <ian6495@gmail.com>
2016-09-19 11:57:34 -0500

    Removed proprietary content

--- FindMountedDrives.ps1
+++
@@ -1,3 +0,0 @@
-﻿$file = "C:\Users\ian.martinez\Desktop\comp.txt"
-$comp = Get-Content -Path $file
-$comp | ForEach-Object -Process {Invoke-Command -ComputerName "$_" {Get-WmiObject Win32_MappedLogicalDisk  | select name, providername}}
